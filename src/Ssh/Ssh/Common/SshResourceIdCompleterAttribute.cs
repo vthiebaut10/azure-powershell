522e58b06d6962a68be3eea88e543f7224846e61
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-08-01 15:14:54 -0400

    Add SSH comdlets

---
+++ src/Ssh/Ssh/Common/SshResourceIdCompleterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.Ssh.Common
+{
+    class SshResourceIdCompleterAttribute : ArgumentCompleterAttribute
+    {
+        public SshResourceIdCompleterAttribute(string [] resourceTypes) : base(CreateScriptBlock(resourceTypes))
+        {
+        }
+
+        public static ScriptBlock CreateScriptBlock(string [] resourceTypes)
+        {
+            string script = "param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)\n";
+            script += "$resourceIds = @()\n";
+            foreach (var resourceType in resourceTypes)
+            {
+                script += $"$resourceType = \"{resourceType}\"\n" +
+                    "$resourceIds = $resourceIds + [Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters.ResourceIdCompleterAttribute]::GetResourceIds($resourceType)\n";
+            }
+            script += "$resourceIds | Where-Object { $_ -Like \"*$wordToComplete*\" } | Sort-Object | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
+            var scriptBlock = ScriptBlock.Create(script);
+            return scriptBlock;
+        }
+    }
+}
---
+++ src/Ssh/Ssh/Common/Track2HybridConnectivityManagementClient.cs
@@ -0,0 +1,40 @@
+﻿using Azure.ResourceManager.HybridConnectivity;
+using Azure.ResourceManager.HybridConnectivity.Models;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.Ssh.Common
+{
+    internal class Track2HybridConnectivityManagementClient
+    {
+        private ArmClient _armClient;
+        private string _subscription;
+        private IClientFactory _clientFactory;
+
+        public Track2HybridConnectivityManagementClient(IClientFactory clientFactory, IAzureContext context)
+        {
+            _clientFactory = clientFactory;
+            _armClient = _clientFactory.CreateArmClient(context, AzureEnvironment.Endpoint.ActiveDirectoryServiceEndpointResourceId);
+            _subscription = context.Subscription.Id;
+        }
+
+        private ResourceGroupResource GetResourceGroup(string resourceGroupName) =>
+            _armClient.GetResourceGroupResource(ResourceGroupResource.CreateResourceIdentifier(_subscription, resourceGroupName));
+
+        public TargetResourceEndpointAccess GetRelayInformationString(string resourceGroup, string resourceName, string endpoint)
+        {
+            return GetRelayInformationString($"/subscriptions/{_subscription}/resourceGroups/{resourceGroup}/providers/Microsoft.HybridCompute/machines/{resourceName}", endpoint);
+        }
+
+        public TargetResourceEndpointAccess GetRelayInformationString(string ResourceId, string endpoint)
+        {
+            EndpointResource myEndpoint = _armClient.GetEndpointResource(EndpointResource.CreateResourceIdentifier(ResourceId, endpoint));
+            var myCred = myEndpoint.GetCredentials(3600);
+            return myCred.Value;
+        }
+        
+
+    }
+}
